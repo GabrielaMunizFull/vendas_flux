0e4fca5653e5877c3f7bddbe0efb3618994aa853
Gabriela Muniz <gabrielasdsmuniz@gmail.com>
2025-05-10 03:34:47 -0300

    first commit

---
+++ src/main/java/com/vendasflux/VendasFluxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vendasflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasFluxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendasFluxBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vendasflux/VendasFluxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vendasflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasFluxBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
